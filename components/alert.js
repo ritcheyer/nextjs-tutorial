a548c98df8a6c4cc5efbe56e5a40326c8f54166c
Eric Ritchey <eritchey@tesla.com>
2022-07-06 10:10:00 -0700

    NextJS tutorial - fetching external data (from the file system and pre-rendering it to the page

--- components/alert.js
+++ components/alert.js
@@ -1,7 +1,7 @@
 import styles from './alert.module.css';
 import cn from 'classnames';
 
-export default function Alert(children, type) {
+export default function Alert({children, type}) {
   return (
     <div
       className={cn({
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-import Link from 'next/link'
-
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
+
+// Importing Styles
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home() {
+
+export default function Home({allPostsData}) {
   return (
    <Layout home>
       <Head>
@@ -13,6 +15,30 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>Hello.</p>
       </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    }
+  }
 }
